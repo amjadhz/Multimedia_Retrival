794edeb6b2684dd5413a0532f7734632ef8092ca
samuelbown <122439091+samuelbown@users.noreply.github.com>
2025-09-25 15:47:16 +0200

    changed visualization function to draw instead of draw_geometry because of shading issues

--- obj_viewer.py
+++ obj_viewer.py
@@ -1,19 +1,28 @@
 import open3d as o3d
 import numpy as np
 
-mesh_path = "data\Apartment\D00045.obj"
+mesh_path = "data\Starship\m1386.obj"
 
 mesh = o3d.io.read_triangle_mesh(mesh_path)
-mesh.compute_vertex_normals()
 
 vertices = np.asarray(mesh.vertices)
 triangles = np.asarray(mesh.triangles)
 
-vis_option = "wireframe_on_shaded"
+print(vertices.shape[0])
+print(triangles.shape[0])
+
+vis_option = "smoothshade"
 if vis_option == "smoothshade":
-    o3d.visualization.draw_geometries([mesh], width=1280, height=720)
+    mesh.compute_vertex_normals()
+    o3d.visualization.draw([mesh], width=1280, height=720, show_skybox=False)
+elif vis_option == "flatshade":
+    mesh.compute_triangle_normals()
+    o3d.visualization.draw([mesh], width=1280, height=720, show_skybox=False)
 elif vis_option == "wireframe_on_shaded":
-    o3d.visualization.draw_geometries([mesh], width=1280, height=720, mesh_show_wireframe=True)
+    wireframe = o3d.geometry.LineSet.create_from_triangle_mesh(mesh) 
+    o3d.visualization.draw([mesh, wireframe], width=1280, height=720, show_skybox=False)
 elif vis_option == "wireframe":
     wireframe = o3d.geometry.LineSet.create_from_triangle_mesh(mesh) 
-    o3d.visualization.draw_geometries([wireframe], width=1280, height=720)
+    o3d.visualization.draw([wireframe], width=1280, height=720, show_skybox=False)
+elif vis_option == "unshaded":
+    o3d.visualization.draw([mesh], width=1280, height=720, show_skybox=False)
